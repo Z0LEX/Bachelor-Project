5795398f90683c43ad769225787f210cd7a6f8b6
Z0LEX <49996142+Z0LEX@users.noreply.github.com>
2023-03-04 13:38:59 +0100

    change main to new graph viewer

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -1,45 +1,19 @@
 package application;
 
-import controllers.TestLayout;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.layout.AnchorPane;
-import org.jspace.ActualField;
-import org.jspace.FormalField;
-import org.jspace.SequentialSpace;
-import org.jspace.Space;
-
 import javafx.application.Application;
-
-import javafx.scene.Scene;
 import javafx.stage.Stage;
-import java.io.IOException;
 
 public class Main extends Application {
 
-    public static void main(String[] args) throws InterruptedException {
-        Space inbox = new SequentialSpace();
-        inbox.put("Hello World!", false);
-        Object[] tuple = inbox.get(new FormalField(String.class), new ActualField(false));
-        System.out.println(tuple[0] + " " + tuple[1]);
+    public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) {
-        FXMLLoader loader = new FXMLLoader(TestLayout.class.getResource("/test-layout.fxml"));
-        try {
-            AnchorPane layout = loader.load();
-            TestLayout test = loader.getController();
-            test.knap1.setOnAction(e -> {
-                test.testfunktion();
-            });
-
-            primaryStage.setScene(new Scene(layout));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        primaryStage.setTitle("Hello World!");
-        primaryStage.show();
+    public void start(Stage stage) {
+        stage.setTitle("Title");
+        stage.setResizable(false);
+        GraphViewer graphViewer = new GraphViewer();
+        graphViewer.startGraphViewer(stage);
     }
 }

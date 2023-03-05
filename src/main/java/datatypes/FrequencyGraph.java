fc1761bf977c53485863314d8b526977cbc4a30b
Z0LEX <49996142+Z0LEX@users.noreply.github.com>
2023-03-05 11:52:40 +0100

    Refactor graph to frequencyGraph and create sumFunction

---
+++ src/main/java/datatypes/FrequencyGraph.java
@@ -0,0 +1,57 @@
+package datatypes;
+
+import java.util.ArrayList;
+import java.util.function.Function;
+
+import javafx.scene.chart.XYChart;
+
+
+public class FrequencyGraph implements Graph {
+
+    private XYChart<Double, Double> graph;
+    private ArrayList<Function> functions;
+    private double range;
+
+    public FrequencyGraph(XYChart<Double, Double> graph, final double range) {
+        this.graph = graph;
+        this.range = range;
+        functions = new ArrayList<>();
+    }
+
+    public void plot(Function<Double, Double> function) {
+        functions.add(function);
+        XYChart.Series<Double, Double> series = new XYChart.Series<Double, Double>();
+        for (double x = 0; x <= range; x = x + 0.01) {
+            plotPoint(x, function.apply(x), series);
+        }
+        graph.getData().add(series);
+    }
+
+    public void combinePlots() {
+        XYChart.Series<Double, Double> series = new XYChart.Series<Double, Double>();
+        EulerGraph.function = sumFunction;
+        for (double x = 0; x <= range; x = x + 0.01) {
+            plotPoint(x, EulerGraph.function.apply(x), series);
+        }
+        graph.getData().add(series);
+    }
+
+    public void plotPoint(double x, double y,
+                          XYChart.Series<Double, Double> series) {
+        XYChart.Data<Double, Double> data = new XYChart.Data<>(x, y);
+        series.getData().add(data);
+    }
+
+    public void clear() {
+        graph.getData().clear();
+        functions.clear();
+    }
+
+    Function<Double, Double> sumFunction = x -> {
+        double sum = 0;
+        for (Function<Double, Double> function : functions) {
+            sum += function.apply(x);
+        }
+        return sum;
+    };
+}

afa05a5ca7c9d77c8d57d075b6ad3f3e88d48a74
Z0LEX <49996142+Z0LEX@users.noreply.github.com>
2023-03-05 11:57:28 +0100

    Add euler and frequency view together

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -1,6 +1,9 @@
 package application;
 
 import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -14,6 +17,13 @@ public class Main extends Application {
         stage.setTitle("Title");
         stage.setResizable(false);
         GraphViewer graphViewer = new GraphViewer();
-        graphViewer.startGraphViewer(stage);
+        EulerViewer eulerViewer = new EulerViewer();
+        Parent graphRoot = graphViewer.getRoot();
+        Parent eulerRoot = eulerViewer.getRoot();
+        BorderPane pane = new BorderPane();
+        pane.setLeft(graphRoot);
+        pane.setRight(eulerRoot);
+        stage.setScene(new Scene(pane));
+        stage.show();
     }
 }

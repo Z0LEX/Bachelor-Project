143b919cb276a69083fd4cb5015463627240daa9
Z0LEX <49996142+Z0LEX@users.noreply.github.com>
2023-03-05 11:52:21 +0100

    refactor graph to an interface

--- src/main/java/datatypes/Graph.java
+++ src/main/java/datatypes/Graph.java
@@ -1,52 +1,15 @@
 package datatypes;
 
-import java.util.ArrayList;
-import java.util.function.Function;
 import javafx.scene.chart.XYChart;
 
-public class Graph {
+import java.util.function.Function;
 
-    private XYChart<Double, Double> graph;
-    private ArrayList<Function> functions;
-    private double range;
+public interface Graph {
 
-    public Graph(final XYChart<Double, Double> graph, final double range) {
-        this.graph = graph;
-        this.range = range;
-        functions = new ArrayList<>();
-    }
+    void plot(Function<Double, Double> function);
 
-    public void plot(final Function<Double, Double> function) {
-        functions.add(function);
-        final XYChart.Series<Double, Double> series = new XYChart.Series<Double, Double>();
-        for (double x = 0; x <= range; x = x + 0.01) {
-            plotPoint(x, function.apply(x), series);
-        }
-        graph.getData().add(series);
-    }
+    void plotPoint(double x, double y, XYChart.Series<Double, Double> series);
 
-    public void combinePlots() {
-        final XYChart.Series<Double, Double> series = new XYChart.Series<Double, Double>();
-
-        for (double x = 0; x <= range; x = x + 0.01) {
-            Double tempX = 0.;
-            for (Function<Double, Double> function : functions) {
-                tempX += function.apply(x);
-            }
-            if (!functions.isEmpty()) {
-                plotPoint(x, tempX, series);
-            }
-        }
-        graph.getData().add(series);
-    }
-
-    private void plotPoint(final double x, final double y,
-                           final XYChart.Series<Double, Double> series) {
-        series.getData().add(new XYChart.Data<Double, Double>(x, y));
-    }
-
-    public void clear() {
-        graph.getData().clear();
-        functions.clear();
-    }
+    void clear();
 }
+

dc9ef662370c5afe14ab33ee74f2264705fccab6
Z0LEX <49996142+Z0LEX@users.noreply.github.com>
2023-03-14 15:31:08 +0100

    Change frequency buttons to combination lock

--- src/main/java/controllers/AddWavesController.java
+++ src/main/java/controllers/AddWavesController.java
@@ -1,11 +1,13 @@
 package controllers;
 
+import components.CombinationLock;
 import components.Wave;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.SplitPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 import java.net.URL;
@@ -15,88 +17,106 @@ import java.util.ResourceBundle;
 import java.util.function.Function;
 
 public class AddWavesController implements Initializable {
-    private ArrayList<Wave> waves = new ArrayList<>();
-
-    private Function<Double, Double> sum;
+    private ArrayList<Wave> waves = new ArrayList<>(4);
 
     private Wave sumWave;
+
+    private CombinationLock lock = new CombinationLock();
+
+    private Label lockNumber1;
+    private Label lockNumber2;
+    private Label lockNumber3;
+    private Label lockNumber4;
     @FXML
-    private Button oneHertz;
-    @FXML
-    private Button twoHertz;
-    @FXML
-    private Button threeHertz;
-    @FXML
-    private Button fourHertz;
-    @FXML
-    private Button fiveHertz;
-    @FXML
-    private Button clearButton;
+    private HBox lockContainer;
     @FXML
     private VBox waveContainer;
     @FXML
     private SplitPane splitPane;
 
     @FXML
-    void handleOneHertz(ActionEvent event) {
-        addWave(1);
-    }
+    private AnchorPane graph1;
 
     @FXML
-    void handleTwoHertz(ActionEvent event) {
-        addWave(2);
-
-    }
+    private AnchorPane graph2;
 
     @FXML
-    void handleThreeHertz(ActionEvent event) {
-        addWave(3);
-
-    }
+    private AnchorPane graph3;
 
     @FXML
-    void handleFourHertz(ActionEvent event) {
-        addWave(4);
-
-    }
+    private AnchorPane graph4;
 
     @FXML
-    void handleFiveHertz(ActionEvent event) {
-        addWave(5);
-
-    }
+    private AnchorPane resultGraphBottom;
 
     @FXML
-    void handleClear(ActionEvent event) {
-        for (Wave wave : waves) {
-            waveContainer.getChildren().remove(wave.getRoot());
-        }
-        waves.clear();
-        sumWave.clear();
-    }
+    private AnchorPane resultGraphTop;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        for (int i = 0; i < 4; i++) {
+            Wave wave = new Wave(0);
+            waves.add(wave);
+            updateWaveContainer(wave, i);
+        }
+
         // Initialize the bottom splitpane with a complicated wave
         Wave wave1 = new Wave(1);
         Wave wave2 = new Wave(3);
         Wave wave3 = new Wave(5);
-        ArrayList<Wave> waveResult = new ArrayList<>(Arrays.asList(wave1, wave2 ,wave3));
+        ArrayList<Wave> waveResult = new ArrayList<>(Arrays.asList(wave1, wave2, wave3));
         Wave result = new Wave(sumWaves(waveResult));
-        splitPane.getItems().remove(1);
-        splitPane.getItems().add(result.getRoot());
+        updateSplitPane(result, 1);
+
+        lockContainer.getChildren().add(lock.getRoot());
+        lockNumber1 = lock.getController().getWheel1Number();
+        lockNumber2 = lock.getController().getWheel2Number();
+        lockNumber3 = lock.getController().getWheel3Number();
+        lockNumber4 = lock.getController().getWheel4Number();
+        lockNumber1.textProperty().addListener((observable, oldValue, newValue) -> {
+            int index = 0;
+            Wave wave = addWave(Double.parseDouble(newValue), index);
+            updateWaveContainer(wave, index);
+        });
+        lockNumber2.textProperty().addListener((observable, oldValue, newValue) -> {
+            int index = 1;
+            Wave wave = addWave(Double.parseDouble(newValue), index);
+            updateWaveContainer(wave, index);
+        });
+        lockNumber3.textProperty().addListener((observable, oldValue, newValue) -> {
+            int index = 2;
+            Wave wave = addWave(Double.parseDouble(newValue), index);
+            updateWaveContainer(wave, index);
+        });
+        lockNumber4.textProperty().addListener((observable, oldValue, newValue) -> {
+            int index = 3;
+            Wave wave = addWave(Double.parseDouble(newValue), index);
+            updateWaveContainer(wave, index);
+        });
     }
 
-    public void addWave(double frequency) {
+    public Wave addWave(double frequency, int index) {
         Wave newWave = new Wave(frequency);
-        waves.add(newWave);
-        waveContainer.getChildren().add(newWave.getRoot());
+        waves.set(index, newWave);
 
-        sum = sumWaves(waves);
-        sumWave = new Wave(sum);
-        splitPane.getItems().remove(0);
-        splitPane.getItems().add(0, sumWave.getRoot());
+        getSumWave();
+        return newWave;
     }
+
+    private void updateWaveContainer(Wave wave, int index) {
+        waveContainer.getChildren().set(index, wave.getRoot());
+    }
+
+    private void updateSplitPane(Wave wave, int index) {
+        splitPane.getItems().set(index, wave.getRoot());
+    }
+
+    private void getSumWave() {
+        Function<Double, Double> sum = sumWaves(waves);
+        sumWave = new Wave(sum);
+        updateSplitPane(sumWave, 0);
+    }
+
 
     private Function<Double, Double> sumWaves(ArrayList<Wave> waves) {
         return x -> {

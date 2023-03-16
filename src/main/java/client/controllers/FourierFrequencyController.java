9bbd72171ec43afafd7f9cac03db46a2f2f9ba8f
KTLDTU <72263629+KTLDTU@users.noreply.github.com>
2023-03-16 12:28:48 +0100

    Create view for client center of mass with stick

---
+++ src/main/java/client/controllers/FourierFrequencyController.java
@@ -0,0 +1,11 @@
+package client.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.shape.Circle;
+
+public class FourierFrequencyController {
+
+    @FXML
+    private Circle center_of_mass;
+
+}

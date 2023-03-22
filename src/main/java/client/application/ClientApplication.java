792fe91e901bdf132cdacc266ad40148baed0eee
Z0LEX <49996142+Z0LEX@users.noreply.github.com>
2023-03-22 18:28:54 +0100

    Add temp scene to client

--- src/main/java/client/application/ClientApplication.java
+++ src/main/java/client/application/ClientApplication.java
@@ -2,16 +2,22 @@ package client.application;
 
 import javafx.application.Application;
 import javafx.collections.ObservableList;
+import javafx.geometry.Pos;
 import javafx.geometry.Rectangle2D;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 import org.jspace.RemoteSpace;
+
 import java.io.IOException;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 
 public class ClientApplication extends Application {
     private static final String PORT = ":9001";
@@ -24,10 +30,17 @@ public class ClientApplication extends Application {
 
     private RemoteSpace clientSpace;
 
+    private int screenIndex = 0;
+
+    private static final int WINDOW_WIDTH = 1024;
+    private static final int WINDOW_HEIGHT = 768;
+
     @Override
     public void start(Stage stage) {
         stage.setTitle("Client title");
         stage.setResizable(false);
+        stage.setWidth(WINDOW_WIDTH);
+        stage.setHeight(WINDOW_HEIGHT);
         // Only place client application on 2nd monitor if there's more than 1 screen
         ObservableList<Screen> screens = Screen.getScreens();
         if (screens.size() > 1) {
@@ -47,7 +60,43 @@ public class ClientApplication extends Application {
             throw new RuntimeException(e);
         }
         FourierFrequencyViewer fourierFrequencyViewer = new FourierFrequencyViewer(clientSpace);
-        fourierFrequencyViewer.startFourierFrequencyViewer(stage);
+        Parent fourierFrequencyRoot = fourierFrequencyViewer.getRoot();
+
+        PhaseShiftViewer phaseShiftViewer = new PhaseShiftViewer(clientSpace);
+        Parent phaseShiftRoot = phaseShiftViewer.getRoot();
+
+        ArrayList<Parent> parents = new ArrayList<>();
+        parents.add(fourierFrequencyRoot);
+        parents.add(phaseShiftRoot);
+
+        BorderPane tempScene = setupTempScene(parents);
+
+        stage.setScene(new Scene(tempScene));
+        stage.show();
+    }
+
+    private BorderPane setupTempScene(ArrayList<Parent> parents) {
+        // Temp scene
+        BorderPane pane = new BorderPane();
+        pane.setCenter(parents.get(screenIndex));
+        Button nextScreenButton = new Button("Next");
+        nextScreenButton.alignmentProperty().set(Pos.CENTER);
+        nextScreenButton.setOnAction(actionEvent -> {
+            if (screenIndex < parents.size() - 1) {
+                screenIndex++;
+                pane.setCenter(parents.get(screenIndex));
+            }
+        });
+        Button prevScreenButton = new Button("Previous");
+        prevScreenButton.setOnAction(actionEvent -> {
+            if (screenIndex > 0) {
+                screenIndex--;
+                pane.setCenter(parents.get(screenIndex));
+            }
+        });
+        pane.setLeft(prevScreenButton);
+        pane.setRight(nextScreenButton);
+        return pane;
     }
 
     // Get the outgoing IP address

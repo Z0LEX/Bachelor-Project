8b19de4bdb1b05fee886f7ac8ff9b7135fd7f926
Z0LEX <49996142+Z0LEX@users.noreply.github.com>
2023-05-08 17:55:08 +0200

    Delete viewer classes and temp scene

--- src/main/java/main/application/Main.java
+++ src/main/java/main/application/Main.java
@@ -2,10 +2,6 @@ package main.application;
 
 import client.application.ClientApplication;
 import javafx.application.Application;
-import javafx.geometry.Pos;
-import javafx.scene.Parent;
-import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
@@ -14,8 +10,6 @@ public class Main extends Application {
     // Screen resolution of touchscreen
     private static final int WINDOW_WIDTH = 1024;
     private static final int WINDOW_HEIGHT = 768;
-
-    private int screenIndex = 0;
 
     private StageManager stageManager;
     public static Stage clientStage;
@@ -39,35 +33,6 @@ public class Main extends Application {
 
         Server server = new Server();
 
-        /*
-
-
-        AddWavesViewer addWavesViewer = new AddWavesViewer(stage);
-        Parent addWavesRoot = addWavesViewer.getRoot();
-
-        AddWavesAmplitudeViewer addWavesAmplitudeViewer = new AddWavesAmplitudeViewer(stage);
-        Parent addWavesAmplitudeRoot = addWavesAmplitudeViewer.getRoot();
-
-        PhaseShiftViewer phaseShiftViewer = new PhaseShiftViewer(stage);
-        Parent phaseShiftRoot = phaseShiftViewer.getRoot();
-
-        FourierMachineViewer fourierMachineViewer = new FourierMachineViewer(stage);
-        Parent fourierMachineRoot = fourierMachineViewer.getRoot();
-
-        DrawGraphViewer drawGraphViewer = new DrawGraphViewer();
-        Parent drawGraphRoot = drawGraphViewer.getRoot();
-
-        ArrayList<Parent> screens = new ArrayList<>();
-        screens.add(addWavesRoot);
-        screens.add(addWavesAmplitudeRoot);
-        screens.add(drawGraphRoot);
-        screens.add(fourierMachineRoot);
-        screens.add(phaseShiftRoot);
-
-        BorderPane tempScene = setupTempScene(screens);
-
-        stage.setScene(new Scene(tempScene));
-        */
         ArrayList<Stage> stages = new ArrayList<>();
         clientStage = new Stage();
         stages.add(stage);
@@ -75,31 +40,7 @@ public class Main extends Application {
 
         setCloseAllStagesOnExit(stages);
 
-//        new ClientApplication().start(clientStage);
-    }
-
-    public BorderPane setupTempScene(ArrayList<Parent> screens) {
-        // Temp scene
-        BorderPane pane = new BorderPane();
-        pane.setCenter(screens.get(screenIndex));
-        Button nextScreenButton = new Button("Next");
-        nextScreenButton.alignmentProperty().set(Pos.CENTER);
-        nextScreenButton.setOnAction(actionEvent -> {
-            if (screenIndex < screens.size() - 1) {
-                screenIndex++;
-                pane.setCenter(screens.get(screenIndex));
-            }
-        });
-        Button prevScreenButton = new Button("Previous");
-        prevScreenButton.setOnAction(actionEvent -> {
-            if (screenIndex > 0) {
-                screenIndex--;
-                pane.setCenter(screens.get(screenIndex));
-            }
-        });
-        pane.setLeft(prevScreenButton);
-        pane.setRight(nextScreenButton);
-        return pane;
+        new ClientApplication().start(clientStage);
     }
 
     // Loop through all stages and add listener to each that closes all stages
